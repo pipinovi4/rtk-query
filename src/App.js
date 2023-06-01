e84e8a8cbc912e0f7779fa0c6ee67d28300f1435
Nikita Bozhenko <prustolivgra@gmail.com>
2023-06-01 19:57:50 +0300

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
+import { useState } from "react";
+import { useAddProductMutation, useGetGoodsQuery } from "./redux";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState('')
+  const [newProduct, setNewProduct] = useState('')
+  const {data = [], iseLoading} = useGetGoodsQuery(count)
+  const [addProduct, {isError}] = useAddProductMutation();
+
+  const handleAddProduct = async ()=> {
+    if (newProduct) {
+      await addProduct({name: newProduct}).unwrap()
+      setNewProduct('')
+    }
+  }
+
+  if (iseLoading) return <h2>Loading...</h2>
+
   return (
-    <div className="App">
-
+    <div>
+      <div>
+        <input type="text" value={newProduct} onChange={(e) => setNewProduct(e.target.value)}/>
+        <button onClick={() => handleAddProduct()}>addProduct </button>
+      </div> 
+      <div>
+        <select value={count} onChange={(e) => setCount(e.target.value)}>
+          <option value="''">all</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/redux/goodApi.js
+++ src/redux/goodApi.js
@@ -1,15 +1,22 @@
-import { createApi, fetchBAseQuery, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const goodsApi = createApi({
-    reducerPath: 'goodsApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3001/'
+  reducerPath: "goodsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3001/",
+  }),
+  endpoints: (build) => ({
+    getGoods: build.query({
+      query: (limit = "") => `goods?${limit && `_limit=${limit}`}`,
     }),
-    endpoints: (build) => ({
-        getGoods: build.query({
-            query: () => 'goods',
-        })
-    })
-})
+    addProduct: build.mutation({
+      query: (body) => ({
+        url: "goods",
+        method: "POST",
+        body,
+      }),
+    }),
+  }),
+});
 
-export const { useGetGoodsQuery} = goodsApi
+export const { useGetGoodsQuery, useAddProductMutation } = goodsApi;
