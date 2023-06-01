9f2c33b3d294047aa677df337455a1f7eb0f3d5f
Nikita Bozhenko <prustolivgra@gmail.com>
2023-06-01 14:50:13 +0300

    first commit

---
+++ src/redux/goodApi.js
@@ -0,0 +1,8 @@
+import { createApi, fetchBAseQuery, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const goodsApi = createApi({
+    reducerPath: 'goodsApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 
+    })
+})
